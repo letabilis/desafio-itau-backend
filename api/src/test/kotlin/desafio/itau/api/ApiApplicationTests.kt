e3ed51ef475c5b1c45df353ccb75253e6ad2dcba
sDowlani <sahilvdowlani@outlook.com>
2025-04-18 16:37:24 -0400

    First Commit: Spring Initialzr

---
+++ api/src/test/kotlin/desafio/itau/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package desafio.itau.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
