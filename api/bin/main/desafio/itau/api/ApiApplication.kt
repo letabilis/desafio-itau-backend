44e6cff19222ec2573e6c783431998f7049115bd
letabilis <sahilvdowlani@outlook.com>
2025-04-19 14:58:43 -0400

    fix blockingQueue bean

--- api/bin/main/desafio/itau/api/ApiApplication.kt
+++ api/bin/main/desafio/itau/api/ApiApplication.kt
@@ -1,20 +1,10 @@
 package desafio.itau.api
 
-import desafio.itau.api.dto.TransacaoDTO
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.LinkedBlockingQueue
 
 @SpringBootApplication
-class ApiApplication {
-
-	@Bean
-	fun blockingQueue(): BlockingQueue<TransacaoDTO> {
-		return LinkedBlockingQueue()
-	}
-}
+class ApiApplication
 
 fun main(args: Array<String>) {
 	runApplication<ApiApplication>(*args)
